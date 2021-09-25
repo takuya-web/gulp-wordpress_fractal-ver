4288e16667718c01c71e2a08ba2895469b1fa61e
mito <raimiti8@gmail.com>
2021-09-26 00:40:09 +0900

    pathの修正

--- production/php/functions.php
+++ production/php/functions.php
@@ -3,7 +3,7 @@
 	//  フォント・CSS・JavaScriptの呼び出し
 	//*****************************************************************
   function mysite_script() {
-    wp_enqueue_style( 'style', get_theme_file_uri( '/css/style.css'), array(), '1.0.0' );
-    wp_enqueue_script( 'scriptjs', get_theme_file_uri( '/js/script.js'), array(), '1.0.0', true );
+    wp_enqueue_style( 'style', get_theme_file_uri( './css/style.css'), array(), '1.0.0' );
+    wp_enqueue_script( 'scriptjs', get_theme_file_uri( './js/script.js'), array(), '1.0.0', true );
   }
   add_action('wp_enqueue_scripts','mysite_script');
